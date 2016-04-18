e41701c23e18295a56437153ddb8531c3d067eaf
Luke Evers <me@lukevers.com>
2016-04-17 20:51:31 -0400

    Condense suite files into one.

--- suite.go
+++ suite.go
@@ -1,8 +1,13 @@
 package ghostinspector
 
 import (
+	"fmt"
 	"time"
 )
+
+type SuiteService struct {
+	client *Client
+}
 
 type Suite struct {
 	ID           string `json:"_id"`
@@ -13,4 +18,39 @@ type Suite struct {
 	DateCreated time.Time `json:"dateCreated"`
 	TestCount   int       `json:"testCount"`
 	Name        string    `json:"name"`
+}
+
+// The response struct for calling List
+type SuiteListResponse struct {
+	Code string  `json:"code"`
+	Data []Suite `json:"data"`
+}
+
+// Fetch an array of all the suites in your account.
+//
+// https://ghostinspector.com/docs/api/suites/#list
+func (s *SuiteService) List() (*SuiteListResponse, error) {
+	res := SuiteListResponse{}
+	_, err := s.client.Get("/suites", []string{}, &res)
+	return &res, err
+}
+
+// The response struct when calling Get
+type SuiteGetResponse struct {
+	Code string `json:"code"`
+	Data Suite  `json:"data"`
+}
+
+// Fetch a single suite from your account.
+//
+// https://ghostinspector.com/docs/api/suites/#get
+func (s *SuiteService) Get(id string) (*SuiteGetResponse, error) {
+	res := SuiteGetResponse{}
+	_, err := s.client.Get(
+		fmt.Sprintf("/suites/%s/", id),
+		[]string{},
+		&res,
+	)
+
+	return &res, err
 }
--- suites.go
+++
@@ -1,5 +0,0 @@
-package ghostinspector
-
-type SuiteService struct {
-	client *Client
-}
