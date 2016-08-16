ddfa5b6b1d1b48632089ee6f1f36f841c5559a78
Luke Evers <luke@lukevers.com>
2016-08-16 09:01:59 -0400

    Added some more talk in doc.go

--- doc.go
+++ doc.go
@@ -1,13 +1,27 @@
 /*
-Package ghostinspector provides a basic API wrapper around the Ghost Inspector API.
+Package ghostinspector provides a basic API wrapper around the Ghost Inspector
+API.
 
 Usage:
 
 	import "github.com/lukevers/ghostinspector"
 
-Construct a new `GhostInspector` client. You will need an API key, which you can
-get from your dashboard.
+Construct a new `GhostInspector` client. You will need an API key, which you
+can get from your dashboard.
 
-	gi := ghostinspector.New("api_key")
+	client := ghostinspector.New("api_key")
+
+Once you have a client, you can do things like find a specific suite or list
+all suites you have access to.
+
+	suites, err := client.ListSuites()
+	if err != nil {
+		// Handle...
+	}
+
+	suite, err := client.GetSuite("longsuiteid")
+	if err != nil {
+		// Handle...
+	}
 */
 package ghostinspector
