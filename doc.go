484321494ec6e8e30dae111ab9fcd3afb93c2b24
Luke Evers <luke@lukevers.com>
2016-08-16 08:32:23 -0400

    Add a basic doc.go file
    More examples/usage to come.

---
+++ doc.go
@@ -0,0 +1,14 @@
+package ghostinspector
+
+/*
+This package provides a basic API wrapper around the Ghost Inspector API.
+
+Usage:
+
+	import "github.com/lukevers/ghostinspector"
+
+Construct a new `GhostInspector` client. You will need an API key, which you can
+get from your dashboard.
+
+	gi := ghostinspector.New("api_key")
+*/
